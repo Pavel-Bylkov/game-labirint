e99ee2f8b22a353c413eb7e257e6165563a79b6a
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-05-08 15:26:17 +0300

    update

--- maze.py
+++ maze.py
@@ -2,8 +2,6 @@ import pygame as pg
 from random import randint
 from time import time
 
-# ToDo Доделать расстановку стен
-# ToDo Добавить охранников
 # Todo Загрузить музыку для фона, и эффектов - поймал охранник, собрал предмет, Геемовер и Победа
 
 directions = {"up": 0, "down": 180, "left": -90, "right": 90}
@@ -40,6 +38,10 @@ class Player(GameSprite):
             self.rect.y += self.speed
         if pg.sprite.spritecollide(self, walls, dokill=False):
             self.rect.x, self.rect.y = pos
+
+    def collide_guards(self, enemies):
+        if pg.sprite.spritecollide(self, enemies, dokill=False):
+            self.rect.x, self.rect.y = start_x, start_y
 
 
 class Guard(GameSprite):
@@ -115,18 +117,21 @@ class Text:
 class Timer(Text):
     def __init__(self, text, start_time, x, y, fsize, color):
         super().__init__(text, x, y, fsize, color)
-        self.start_time = start_time
+        self.current_time = start_time
         self.last_time = time()
         self.text_prefix = text  # чтобы не перезаписывать каждый раз Тест до значения таймера
 
     def update(self, window2):
-        if self.start_time > 0 and time() - self.last_time > 1:
-            self.start_time -= 1
+        if self.current_time > 0 and time() - self.last_time > 1:
+            self.current_time -= 1
             self.last_time = time()
-        self.text_update(self.text_prefix + str(self.start_time), window2)
+        self.text_update(self.text_prefix + str(self.current_time), window2)
+
+    def add_time(self, new_time):
+        self.current_time += new_time
 
     def is_end(self):
-        return self.start_time == 0
+        return self.current_time == 0
 
 
 pg.init()  # настройка pygame на наше железо, в том числе видео карта, звуковая и установленные шрифты
@@ -141,14 +146,17 @@ background = pg.transform.scale(background, (win_width, win_height))
 font = pg.font.SysFont("Arial", 120)  # подключаем модуль font из pygame и создаем объект Шрифт
 win = font.render("You WIN!!!", True, GREEN)
 lose = font.render("You LOSE!!!", True, RED)
+pause_img = font.render("PAUSE", True, WHITE)
 
-
-hero = Player(img="шар.png", x=45, y=45, size_x=50, size_y=50, speed=10)
+start_x, start_y = 45, 45
+hero = Player(img="шар.png", x=start_x, y=start_y, size_x=50, size_y=50, speed=10)
 guards = pg.sprite.Group()
 guards.add(
     Guard(img="шар.png", x=win_width - 100, y=win_height//2, size_x=50, size_y=50,
               speed=7, direction=-90, start=500, end=win_width - 100)
 )
+eleksir = pg.sprite.Group()
+eleksir.add(GameSprite(img="treasure.png", x=100, y=win_height - 100, size_x=60, size_y=60, speed=10))
 aurum = GameSprite(img="treasure.png", x=win_width - 100, y=win_height - 100, size_x=60, size_y=60, speed=10)
 walls = pg.sprite.Group()
 walls.add(
@@ -168,24 +176,46 @@ timer = Timer(text="Time: ", start_time=30, x=win_width - 150, y=10, fsize=30, c
 clock = pg.time.Clock()
 FPS = 30  # частота срабатывания таймера 30 кадров в секунду
 
+finish = False
+pause = False
 run = True
 while run:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
-    # обновляем координаты и накладываем на экранную поверхность фон и всех спрайтов
-    window.blit(background, (0, 0))  # копирование изображения на экранную поверхность
-    hero.update()
-    guards.update()
-    hero.reset()
-    aurum.reset()
+        if event.type == pg.KEYDOWN and event.key == pg.K_p:
+            if pause:
+                pause = False
+            else:
+                pause = True
+    if not finish and not pause:
+        # обновляем координаты и накладываем на экранную поверхность фон и всех спрайтов
+        window.blit(background, (0, 0))  # копирование изображения на экранную поверхность
+        hero.update()
+        guards.update()
+        hero.reset()
+        aurum.reset()
 
-    guards.draw(window)
-    walls.draw(window)  # вызываем групповой метод копирования изображения каждой стены на экранную поверхность
-    timer.update(window)
+        guards.draw(window)
+        walls.draw(window)  # вызываем групповой метод копирования изображения каждой стены на экранную поверхность
+        eleksir.draw(window)
 
-    if timer.is_end():
-        window.blit(lose, (win_width//2 - 100, win_height//2 - 50))
+        timer.update(window)
+
+        hero.collide_guards(guards)
+
+        if pg.sprite.collide_mask(hero, aurum):
+            finish = True
+            window.blit(win, (win_width // 2 - 100, win_height // 2 - 50))
+
+        if pg.sprite.spritecollide(hero, eleksir, dokill=True):
+            timer.add_time(5)
+
+        if timer.is_end():
+            finish = True
+            window.blit(lose, (win_width//2 - 100, win_height//2 - 50))
+    elif not finish:
+        window.blit(pause_img, (win_width // 2 - 100, win_height // 2 - 50))
     pg.display.update()
 
     clock.tick(FPS)
