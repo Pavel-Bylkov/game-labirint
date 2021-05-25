34b7644e8e4002be92f56d9fa690e75b4c967d2e
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-05-25 17:45:49 +0300

    update move guard

--- maze-ivan.py
+++ maze-ivan.py
@@ -1,15 +1,16 @@
 import pygame as pg
-from random import randint
+
 from time import time
 
 # ToDo Добавить охранникам сканер - если попадаем в поле зрения, бегут за нами
 # Todo Загрузить музыку для фона, и эффектов - поймал охранник, собрал предмет, Геемовер и Победа
 
-
 WHITE = (255, 255, 255)
 BLUE = (45, 62, 202)
 GREEN = (50, 200, 60)
 RED = (150, 30, 30)
+
+win_width, win_height = 1200, 900  # задаем размеры экранной поверхности
 
 
 class GameSprite(pg.sprite.Sprite):
@@ -47,45 +48,73 @@ class Player(GameSprite):
                 self.rect.y = y
 
 
+class Scanner(pg.sprite.Sprite):
+    def __init__(self, x, y, size_x, size_y):
+        super().__init__()
+        self.image = pg.Surface((size_x, size_y))
+        self.image.fill((0, 0, 0))
+        self.rect = self.image.get_rect()  # рамка вокруг картинки
+        self.rect.centerx = x
+        self.rect.centery = y
+
+    def update(self, x, y):
+        self.rect.centerx = x
+        self.rect.centery = y
+
 
 class Guard(GameSprite):
+    # ToDo В дальнейшем добавить разделение на разные маршруты
+    # ToDo Проверить сброс генератора случайных чисел - при создание спрайта
+    position = [(60, 60), (win_width - 110, 60), (win_width//2, win_height//2),
+                (60, win_height - 110), (win_width - 110, win_height - 110),
+                (60, win_height//2), (win_width - 110, win_height//2),
+                (win_width//2, 60), (win_width//2, win_height - 110)]
+
     def __init__(self, img, x, y, size_x, size_y, speed):
         super().__init__(img, x, y, size_x, size_y, speed)
-        self.end_x = randint(10, win_width//5 - 10) * 5  # случайное положение по оси х
-        self.end_y = randint(10, win_height//5 - 10) * 5  # случайное положение по оси у
-        self.last_x, self.last_y = x, y
+        from random import choice
+        self.choice = choice
+        self.end_x, self.end_y = self.choice(Guard.position)
+        self.last_x, self.last_y = None, None
+        self.scanner = Scanner(x=self.rect.centerx, y=self.rect.centery, size_x=size_x*50, size_y=size_x*50)
+        self.scanner_walls = Scanner(x=self.rect.centerx, y=self.rect.centery,
+                                     size_x=size_x, size_y=size_x)
 
+    def update(self, player):
+        if pg.sprite.collide_rect(self.scanner, player):
+            self.scanner_walls.rect = pg.draw.line(window, GREEN,
+                                                   (self.rect.centerx, self.rect.centery),
+                                                   (player.rect.centerx, player.rect.centery))
+            for wall in walls:
+                if pg.sprite.collide_rect(self.scanner_walls, wall):
+                    break
+            else:
+                self.end_x, self.end_y = player.rect.x, player.rect.y
+        while self.rect.collidepoint(self.end_x, self.end_y):
+            self.end_x, self.end_y = self.choice(Guard.position)
+        self.last_x, self.last_y = self.rect.x, self.rect.y
 
-    def update(self):
-        if self.rect.collidepoint(self.end_x, self.end_y):
-            self.end_x = randint(10, win_width // 5 - 10) * 5  # случайное положение по оси х
-            self.end_y = randint(10, win_height // 5 - 10) * 5
-
-        if self.last_y == self.rect.y and self.last_y - self.end_y < 0:
-            self.up_down = "down"
-        elif self.last_y == self.rect.y and self.last_y - self.end_y > 0:
-            self.up_down = "up"
-        if self.last_x == self.rect.x and self.last_x - self.end_x < 0:
-            self.left_right = "right"
-        elif self.last_x == self.rect.x and self.last_x - self.end_x > 0:
-            self.left_right = "left"
         x, y = self.rect.x, self.rect.y
-        if self.up_down == "up":
-            self.rect.y = self.rect.y - self.speed
+        if self.last_y > self.end_y - 3:  # делаем +/- 5 из за зависаний в разных точках
+            self.rect.y -= self.speed
             if pg.sprite.spritecollide(self, walls, dokill=False):
                 self.rect.y = y
-        elif self.up_down == "down":
+        if self.last_y < self.end_y + 3:
             self.rect.y += self.speed
             if pg.sprite.spritecollide(self, walls, dokill=False):
                 self.rect.y = y
-        if self.left_right == "left":
+        if self.last_x > self.end_x - 3:
             self.rect.x -= self.speed
             if pg.sprite.spritecollide(self, walls, dokill=False):
                 self.rect.x = x
-        elif self.left_right == "right":
+        if self.last_x < self.end_x + 3:
             self.rect.x += self.speed
             if pg.sprite.spritecollide(self, walls, dokill=False):
                 self.rect.x = x
+        if self.last_x == self.rect.x and self.last_y == self.rect.y:
+            self.end_x, self.end_y = self.choice(Guard.position)
+        self.scanner.update(x=self.rect.centerx, y=self.rect.centery)
+        self.scanner_walls.update(x=self.rect.centerx, y=self.rect.centery)
 
 
 class Wall(pg.sprite.Sprite):
@@ -150,7 +179,7 @@ class Timer(Text):
 
 pg.init()  # настройка pygame на наше железо, в том числе видео карта, звуковая и установленные шрифты
 pg.font.init()
-win_width, win_height = 1200, 900  # задаем размеры экранной поверхности
+
 window = pg.display.set_mode((win_width, win_height))
 
 background = pg.image.load("fon0.jpg")
@@ -165,8 +194,8 @@ lose = font.render("You LOSE!!!", True, RED)
 hero = Player(img="шар.png", x=45, y=45, size_x=50, size_y=50, speed=10)
 guards = pg.sprite.Group()
 guards.add(
-    Guard(img="шар.png", x=win_width - 100, y=win_height//2, size_x=50, size_y=50, speed=7),
-    Guard(img="шар.png", x=win_width//2, y=win_height//2, size_x=50, size_y=50, speed=7)
+    Guard(img="шар.png", x=win_width - 100, y=win_height//2, size_x=50, size_y=50, speed=4),
+    Guard(img="шар.png", x=win_width//2, y=win_height//2, size_x=50, size_y=50, speed=4)
 )
 aurum = GameSprite(img="treasure.png", x=win_width - 100, y=win_height - 100, size_x=60, size_y=60, speed=10)
 walls = pg.sprite.Group()
@@ -181,6 +210,11 @@ walls.add(
     Wall(x=win_width - 35, y=0, size_x=35, size_y=win_height, color=BLUE),
     Wall(x=112, y=0, size_x=15, size_y=375, color=BLUE),
 )
+setka = pg.sprite.Group()
+for y in range(50, win_height, 50):
+    setka.add(Wall(x=0, y=y, size_x=win_width, size_y=2, color=RED))
+for x in range(50, win_width, 50):
+    setka.add(Wall(x=x, y=0, size_x=2, size_y=win_height, color=RED))
 # window.fill(BLUE)   # заливка экрана одним цветом
 
 timer = Timer(text="Time: ", start_time=30, x=win_width - 150, y=10, fsize=30, color=WHITE)
@@ -197,12 +231,13 @@ while run:
     # window.blit(background, (0, 0))  # копирование изображения на экранную поверхность
     window.fill(GREEN)
     hero.update()
-    guards.update()
+    guards.update(hero)
     hero.reset()
     aurum.reset()
 
     guards.draw(window)
     walls.draw(window)  # вызываем групповой метод копирования изображения каждой стены на экранную поверхность
+    setka.draw(window)
     timer.update(window)
 
     if timer.is_end():
